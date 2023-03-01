04b17ab05ff8716176a0f6f66eda4e7a0e438322
leonardolginfo <leonardo@lginfo.com.br>
2023-03-01 08:29:25 -0300

    declaracao dos atributos e mapeamento

--- app/src/main/java/br/com/lginfo/gamejokempo/MainActivity.java
+++ app/src/main/java/br/com/lginfo/gamejokempo/MainActivity.java
@@ -2,13 +2,29 @@ package br.com.lginfo.gamejokempo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView player1;
+    ImageView player2;
+    ImageButton rock;
+    ImageButton paper;
+    ImageButton cut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player1 = findViewById(R.id.imgPlayer1);
+        player2 = findViewById(R.id.imgPlayer2);
+        rock = findViewById(R.id.imgBtnRock);
+        paper = findViewById(R.id.imgBtnPaper);
+        cut = findViewById(R.id.imgBtnCut);
+
     }
 }
